0d903a439d2567ecd8e13cff177afc9f68536ecd
Allister Grange <allistergrange@gmail.com>
2020-12-30 18:10:00 +1300

    Basic socket.io connection working

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import socketIOClient from "socket.io-client";
+
+
+const ENDPOINT = "http://127.0.0.1:3000";
 
 function App() {
+  const [response, setResponse] = useState("");
+
+  useEffect(() => {
+    const socket = socketIOClient(ENDPOINT);
+    socket.on("FromAPI", (data: SocketIOClient.Socket) => {
+      setResponse(data as unknown as string);      
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {response}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
