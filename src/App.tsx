4952bda0ea12afba2849a49db3e4c76e8b5ec098
Allister Grange <allistergrange@gmail.com>
2021-01-03 16:49:26 +1300

    Very basic homepage outlined

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import socketIOClient from "socket.io-client";
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from './pages/HomePage';
+import LobbyPage from './pages/lobby/LobbyPage';
+import BoardPage from './pages/game/BoardPage';
+import CardPage from './pages/game/CardPage';
 
 const ENDPOINT = "http://127.0.0.1:3000";
 
 function App() {
-  const [response, setResponse] = useState("");
-
-  useEffect(() => {
-    const socket = socketIOClient(ENDPOINT);
-    socket.on("FromAPI", (data: SocketIOClient.Socket) => {
-      setResponse(data as unknown as string);      
-    });
-  }, []);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {response}
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/:roomId" component={LobbyPage} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import './HomePage.css';
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+
+const ENDPOINT = "http://127.0.0.1:3000";
+
+function HomePage() {
+    const [gameName, setGameName] = React.useState("");
+    const [showingJoinGameOptions, setShowingJoinGameOptions] = React.useState(false);
+
+    const handleRoomNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setGameName(event.target.value);
+    };
+
+    //   useEffect(() => {
+    //     const socket = socketIOClient(ENDPOINT);
+    //     socket.on('message', (data: SocketIOClient.Socket) => {
+    //       setResponse(data as unknown as string);   
+    //       console.log(data);
+    //     });
+    //   }, []);
+
+    const generateGameID = () => {
+        return Math.floor(1000 + Math.random() * 9000) - 1;
+    }
+
+    return (
+        <div className="App">
+            <div className="Title">
+                <header className="App-header">
+                    <p>
+                        welcome to pontificate
+                    </p>
+                </header>
+            </div>
+
+            {
+                showingJoinGameOptions ?
+                    <div className="RoomInputForm">
+                        <Input
+                            color="primary"
+                            type="text"
+                            placeholder="Room"
+                            value={gameName}
+                            onChange={handleRoomNameChange}
+                            className="text-input-field"
+                        />
+                        <Button component={Link} to={`/${gameName}`} variant="contained" color="primary">
+                            join game
+                        </Button>
+                        <Button variant="contained" color="primary" onClick={() => { setShowingJoinGameOptions(false) }}>
+                            back
+                        </Button>
+                    </div>
+                    :
+                    <div className="GameOptions">
+                        <Button component={Link} to={`/${generateGameID()}`} variant="contained" color="primary">
+                            create new game
+                        </Button>
+                        <Button variant="contained" color="primary" onClick={() => { setShowingJoinGameOptions(true) }}>
+                            join game
+                        </Button>
+                    </div>
+            }
+        </div>
+    );
+}
+
+export default HomePage;
