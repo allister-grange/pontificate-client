cac0fc463e41ec6ee107038bf4821a339e9684b9
Allister Grange <allistergrange@gmail.com>
2021-02-15 10:52:26 +1300

    Adding in the card displaying on the card page

--- src/pages/game/CardPage.tsx
+++ src/pages/game/CardPage.tsx
@@ -2,14 +2,33 @@ import React, { useEffect, useState } from 'react';
 
 import logo from './logo.svg';
 import socketIOClient from "socket.io-client";
+import './CardPage.css';
 
 function CardPage() {
 
+  const objectsCards = ['Suspension Bridge', 'Syringe', 'Paper Clip', 'Accessory', 
+        'Bill', 'Log', 'Camera', 'Flint', 'Signpost']
+
+  const [indexOfLastCard, setIndexOfLastCard] = useState(-1);
+  const [indexOfCurrentCard, setIndexOfCurrentCard] = useState(-1);
+
+  useEffect(() => {
+    setIndexOfCurrentCard(Math.floor(Math.random() * objectsCards.length));
+  }, []);
+  
+  const changeCards = () => {
+    setIndexOfLastCard(indexOfCurrentCard);
+    setIndexOfCurrentCard(Math.floor(Math.random() * objectsCards.length));
+  }
+
   return (
-    <div>
-     <h1>
-         You are in the card page!!!
-     </h1>
+    <div onClick={changeCards}>
+      <h1>
+        {objectsCards[indexOfCurrentCard]}
+      </h1>
+      <h4>
+        {objectsCards[indexOfLastCard]}
+      </h4>
     </div>
   );
 }
