4b36021a5d3f006e9d03a52d1297a9c9c947c672
Allister Grange <allistergrange@gmail.com>
2021-02-15 10:52:48 +1300

    Trying to work out a circular board on the boardpage

--- src/pages/game/BoardPage.tsx
+++ src/pages/game/BoardPage.tsx
@@ -2,15 +2,52 @@ import React, { useEffect, useState } from 'react';
 
 import logo from './logo.svg';
 import socketIOClient from "socket.io-client";
+import './BoardPage.css';
 
 function BoardPage() {
 
+  useEffect(() => {
+
+  }, [])
+
+  const [progress, useProgress] = useState(0);
+
+  const size = 300;
+  const strokeWidth = 10;
+  const circleOneStroke = 10;
+  const circleTwoStroke = 10;
+
+  const center = size / 2;
+  const radius = size / 2 - strokeWidth / 2;
+  const circumference = 2 * Math.PI * radius;
+
+
   return (
-    <div>
-        <h1>
-            You are in the board page!!!
-        </h1>
-    </div>
+    <>
+      <svg className="svg" width={size} height={size}>
+        <circle
+          className="svg-circle-bg"
+          stroke={'#7ea9e1'}
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={strokeWidth}
+        />
+        <circle
+          className="svg-circle"
+          stroke={'#7ea9e1'}
+          cx={center}
+          cy={center}
+          r={radius}
+          strokeWidth={strokeWidth}
+        />
+        <text className="svg-circle-text" x={center} y={center}>
+          {progress}%
+    </text>
+      </svg>
+
+    </>
+
   );
 }
 
--- src/pages/lobby/HostLobbyPage.tsx
+++ src/pages/lobby/HostLobbyPage.tsx
@@ -9,64 +9,64 @@ const ENDPOINT = "http://127.0.0.1:3000";
 
 const HostLobbyPage = (props: any): any => {
 
-    const { gameId } = props.match.params; // Gets roomId from URL
-    const { players, createNewGame, startGame } = useHostLobby(gameId);
+  const { gameId } = props.match.params; // Gets roomId from URL
+  const { players, createNewGame, startGame } = useHostLobby(gameId);
 
-    useEffect(() => {
-        //nothing
-        console.log(props.match.params);
-        createNewGame(gameId);
-    }, []);
+  useEffect(() => {
+    //nothing
+    console.log(props.match.params);
+    createNewGame(gameId);
+  }, []);
 
-    const allPlayersAreReady = (): boolean => {
-        let allReady = true;
-        players.map((player: any) => {
-            if (!player.isReady) {
-                allReady = false;
-            }
-        });
-        return allReady;
-    }
+  const allPlayersAreReady = (): boolean => {
+    let allReady = true;
+    players.map((player: any) => {
+      if (!player.isReady) {
+        allReady = false;
+      }
+    });
+    return allReady;
+  }
 
-    return (
-        <div>
-            <h1>
-                HOST LOBBY: GAMEID: {gameId}
-            </h1>
+  return (
+    <div>
+      <h1>
+        HOST LOBBY: GAMEID: {gameId}
+      </h1>
+      {
+        players.length === 0 ?
+          < p >
+            no one in the lobby yet g unit
+					</p>
+          :
+          <div>
             {
-                players.length === 0 ?
-                    < p >
-                        no one in the lobby yet g unit
-                    </p>
-                    :
-                    <div>
-                        {
-                            players.map((player: any, index: number) => {
-                                return (
-                                    <p key={index}>{player.userName + ' ' + player.isReady}</p>
-                                )
-                            })
-                        }
-                    </div>
+              players.map((player: any, index: number) => {
+                return (
+                  <p key={index}>{player.userName + ' ' + player.isReady}</p>
+                )
+              })
             }
+          </div>
+      }
 
-            <div>
-                {
-                    allPlayersAreReady() && players.length > 0 ?
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={() => { startGame(gameId); }}
-                            className="button">
-                            Start game
-                        </Button>
-                        :
-                        null
-                }
-            </div>
+      <div>
+        {
+          allPlayersAreReady() && players.length > 0 ?
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => { startGame(gameId); }}
+              className="button">
+              Start game
+						</Button>
+            :
+            null
+        }
+      </div>
 
-        </div >
-    );
+    </div >
+  );
 }
 
 export default HostLobbyPage;
