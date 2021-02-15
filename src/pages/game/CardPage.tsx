cb19072ea669a2d7e5078a4c222336af718e674d
Allister Grange <allistergrange@gmail.com>
2021-02-15 14:32:59 +1300

    Added in the buttons/count for the card page

--- src/pages/game/CardPage.tsx
+++ src/pages/game/CardPage.tsx
@@ -3,32 +3,63 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import socketIOClient from "socket.io-client";
 import './CardPage.css';
+import { Box, Button, Grid } from '@material-ui/core';
+import { borders } from '@material-ui/system';
 
 function CardPage() {
 
-  const objectsCards = ['Suspension Bridge', 'Syringe', 'Paper Clip', 'Accessory', 
-        'Bill', 'Log', 'Camera', 'Flint', 'Signpost']
+  const objectsCards = ['Suspension Bridge', 'Syringe', 'Paper Clip', 'Accessory',
+    'Bill', 'Log', 'Camera', 'Flint', 'Signpost']
 
   const [indexOfLastCard, setIndexOfLastCard] = useState(-1);
   const [indexOfCurrentCard, setIndexOfCurrentCard] = useState(-1);
+  const [correctCount, setCorrectCount] = useState(0);
 
   useEffect(() => {
     setIndexOfCurrentCard(Math.floor(Math.random() * objectsCards.length));
   }, []);
-  
-  const changeCards = () => {
+
+  const nextCard = () => {
+    setCorrectCount(correctCount + 1);
     setIndexOfLastCard(indexOfCurrentCard);
     setIndexOfCurrentCard(Math.floor(Math.random() * objectsCards.length));
   }
 
+  //use a stack?
+  const lastCard = () => {
+    setIndexOfCurrentCard(indexOfLastCard);
+    setIndexOfLastCard(indexOfCurrentCard);
+  }
+
   return (
-    <div onClick={changeCards}>
-      <h1>
-        {objectsCards[indexOfCurrentCard]}
-      </h1>
-      <h4>
-        {objectsCards[indexOfLastCard]}
-      </h4>
+    <div className="root">
+      <div className="words">
+        <h1>
+          {objectsCards[indexOfCurrentCard]}
+        </h1>
+        <h4>
+          {objectsCards[indexOfLastCard]}
+        </h4>
+      </div>
+
+      <div className="correctCardCount">
+        <h4 style={{ paddingRight: '2px'}}>{correctCount}</h4>
+        <p style={{ paddingLeft: '2px'}}>correct cards</p>
+      </div>
+
+      <div className="buttons">
+        <div style={{ paddingRight: '15px' }}>
+          <Button variant="outlined" onClick={lastCard}>
+            incorrect
+          </Button>
+        </div>
+
+        <div style={{ paddingLeft: '15px' }}>
+          <Button variant="outlined" onClick={nextCard}>
+            correct
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
