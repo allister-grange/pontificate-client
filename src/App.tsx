09826be08499b161d56efe451f55b8d1e35aaa4d
Allister Grange <allistergrange@gmail.com>
2021-02-28 18:02:21 +1300

    Adding a theme to the styling

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,24 @@ import PlayerLobbyPage from './pages/lobby/PlayerLobbyPage';
 import HostLobbyPage from './pages/lobby/HostLobbyPage';
 import CardPage from './pages/game/CardPage';
 import BoardPage from './pages/game/BoardPage';
+import { ThemeProvider } from '@material-ui/core/styles'
+import { theme } from './components/theme';
 
 const ENDPOINT = "http://127.0.0.1:3000";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/host-lobby/:gameId" component={HostLobbyPage} />
-        <Route exact path="/player-lobby/:gameId" component={PlayerLobbyPage} />
-        <Route exact path="/host-game/:gameId" component={BoardPage} />
-        <Route exact path="/player-game/:gameId" component={CardPage} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/host-lobby/:gameId" component={HostLobbyPage} />
+          <Route exact path="/player-lobby/:gameId" component={PlayerLobbyPage} />
+          <Route exact path="/host-game/:gameId" component={BoardPage} />
+          <Route exact path="/player-game/:gameId" component={CardPage} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
